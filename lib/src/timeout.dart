5c3702e450dfe3a13413da13abfc267991ce5eae
Therenca Mureithi <therencamureithi@gmail.com>
2022-06-21 07:00:59 +0300

    aqua.Timeout.until null check added

--- lib/src/timeout.dart
+++ lib/src/timeout.dart
@@ -5,22 +5,24 @@ class Timeout {
 	static Future<dynamic> until(int retries, Function callback, {int? milliseconds, bool verbose=true}) async {
 		int count = 0;
 		dynamic value = await callback();
-		while(retries == 0 ||(count < retries && value == null)){
-			if(verbose){
-				pretifyOutput('[${count+1}/$retries] $value', color: AqColor.cyann);
-			}
-			value = await callback();
-			if(value == null){
-				await delay(milliseconds ?? 1000);
-				count++;
-			} else {
-				break;
+		if(value == null){
+			while(retries == 0 ||(count < retries && value == null)){
+				if(verbose){
+					pretifyOutput('[${count+1}/$retries] $value', color: AqColor.cyann);
+				}
+				value = await callback();
+				if(value == null){
+					await delay(milliseconds ?? 1000);
+					count++;
+				} else {
+					break;
+				}
 			}
 		}
 
 		if(verbose){
-				pretifyOutput('[${count+1}/$retries][DONE]$value');
-			}
+			pretifyOutput('[${count+1}/$retries][DONE]$value');
+		}
 		return value;
 	}
 }
